0a84332b0177722771b78d40daf67e900ea9cd1f
David Armendariz <david.adrian.a.p@gmail.com>
2022-01-26 20:23:56 -0500

    Added createRequest function

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -29,4 +29,15 @@ contract Campaign {
     require(msg.value > minimumContribution);
     approvers.push(msg.sender);
   }
+
+  function createRequest(string memory description, uint value, address recipient) public restricted {
+    Request memory newRequest = Request({
+      description: description,
+      value: value,
+      recipient: recipient,
+      complete: false
+    });
+
+    requests.push(newRequest);
+  }
 }
