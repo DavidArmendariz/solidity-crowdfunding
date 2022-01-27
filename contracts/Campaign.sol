3e68ff7bed89008f5dc693076028958a2d6b62f5
David Armendariz <david.adrian.a.p@gmail.com>
2022-01-26 19:49:28 -0500

    Added Request struct

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -3,6 +3,12 @@
 pragma solidity ^0.8.11;
 
 contract Campaign {
+  struct Request {
+    string description;
+    uint value;
+    address recipient;
+    bool complete;
+  }
   address public manager;
   uint minimumContribution;
   address[] public approvers;
