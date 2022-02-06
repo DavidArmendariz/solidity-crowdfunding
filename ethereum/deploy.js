f9d0513195215945a932e22c229840913ff622bd
David Armendariz <david.adrian.a.p@gmail.com>
2022-02-06 16:18:47 -0500

    Fixed deploy.js

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,24 +1,30 @@
 import 'dotenv/config';
 import HDWalletProvider from '@truffle/hdwallet-provider';
 import Web3 from 'web3';
-import Lottery from './lottery';
+import CampaignFactory from './build/CampaignFactory';
 
 const provider = new HDWalletProvider(
   process.env.SEED_PHRASE,
   process.env.NETWORK_URL
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   try {
     const accounts = await web3.eth.getAccounts();
+
     console.log(`Deploying from account: ${accounts[0]}`);
-    const result = await new web3.eth.Contract(Lottery.abi)
+
+    const result = await new web3.eth.Contract(CampaignFactory.abi)
       .deploy({
-        data: Lottery.evm.bytecode.object,
+        data: CampaignFactory.evm.bytecode.object,
       })
       .send({ gas: 1000000, from: accounts[0] });
-    console.log(`Contract ABI: ${JSON.stringify(Lottery.abi, null, 2)}`);
+
+    console.log(
+      `Contract ABI: ${JSON.stringify(CampaignFactory.abi, null, 2)}`
+    );
     console.log(`Contract Address: ${result.options.address}`);
   } catch (err) {
     console.log(`Error: ${err.message}`);
