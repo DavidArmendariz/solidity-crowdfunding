2ed237e5597f14df4b8c36c5cb105f60501b278b
David Armendariz <david.adrian.a.p@gmail.com>
2022-02-05 21:36:35 -0500

    WIP on refactoring

---
+++ ethereum/compile.js
@@ -0,0 +1,7 @@
+import fse from 'fs-extra';
+import path from 'path';
+import solc from 'solc';
+
+console.log(path.resolve());
+// const buildPath = path.resolve(__dirname, 'build');
+// fse.removeSync(buildPath);
