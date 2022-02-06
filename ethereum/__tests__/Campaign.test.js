cb7efaae9f17fe321edb339d296b3b643bedbd5c
David Armendariz <david.adrian.a.p@gmail.com>
2022-02-05 22:55:33 -0500

    WIP on texts

--- ethereum/__tests__/Campaign.test.js
+++ ethereum/__tests__/Campaign.test.js
@@ -1,3 +1,18 @@
-it('tests something', () => {
-  expect(1 + 1).toBe(2);
+import ganache from 'ganache';
+import Web3 from 'web3';
+import CampaignFactory from '../build/CampaignFactory.json';
+import Campaign from '../build/Campaign.json';
+
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  factory = await new web3.eth.Contract(JSON.parse(CampaignFactory.interface))
+    .deploy({ data: CampaignFactory.evm.bytecode.object })
+    .send({ from: accounts[0], gas: 1000000 });
 });
