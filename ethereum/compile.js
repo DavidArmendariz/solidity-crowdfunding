804d291984b162498b7e44bdcdf22c80926a0c3f
David Armendariz <david.adrian.a.p@gmail.com>
2022-02-05 22:22:26 -0500

    Added compile.js to put contracts in build folder

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -1,6 +1,40 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import fse from 'fs-extra';
+import solc from 'solc';
 
-console.log(path.resolve());
-// const buildPath = path.resolve(__dirname, 'build');
-// fse.removeSync(buildPath);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const buildPath = path.resolve(__dirname, 'build');
+fse.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const source = fse.readFileSync(campaignPath, 'utf-8');
+
+const input = {
+  language: 'Solidity',
+  sources: {
+    allContracts: {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
+      },
+    },
+  },
+};
+
+const compiledContract = solc.compile(JSON.stringify(input));
+const output = JSON.parse(compiledContract);
+const contracts = Object.entries(output.contracts.allContracts);
+
+fse.ensureDirSync(buildPath);
+
+contracts.forEach((contract) => {
+  const [contractName, json] = contract;
+  fse.outputJSONSync(path.resolve(buildPath, `${contractName}.json`), json);
+});
