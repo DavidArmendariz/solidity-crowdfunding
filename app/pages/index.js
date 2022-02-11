8ca2547895dfebd2272a7203b168b43d7a088c8c
David Armendariz <david.adrian.a.p@gmail.com>
2022-02-10 21:51:05 -0500

    Added layout component

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,17 +1,28 @@
 import Head from 'next/head';
+import { Button, Card } from 'semantic-ui-react';
+import Layout from '../components/Layout';
 import factory from '../factory';
 
 const Home = ({ campaigns }) => {
+  const renderCampaigns = () => {
+    const items = campaigns.map((campaign) => ({
+      header: campaign,
+      description: <a>View Campaign</a>,
+      fluid: true,
+    }));
+
+    return <Card.Group items={items} />;
+  };
+
   return (
-    <div>
+    <Layout>
       <Head>
         <title>Crowdfunding App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {campaigns.map((campaign) => (
-        <div key={campaign}>{campaign}</div>
-      ))}
-    </div>
+      {renderCampaigns()}
+      <Button content="Create Campaign" icon="add circle" primary />
+    </Layout>
   );
 };
 
