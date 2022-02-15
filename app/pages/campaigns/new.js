71362be60af919e324fdb05da72e8651a72ab4ff
David Armendariz <david.adrian.a.p@gmail.com>
2022-02-14 21:31:54 -0500

    Added new page

--- app/pages/campaigns/new.js
+++ app/pages/campaigns/new.js
@@ -1,10 +1,20 @@
 import Layout from 'components/Layout';
 import React from 'react';
+import { Button, Form } from 'semantic-ui-react';
 
 const CampaignNew = () => {
   return (
     <Layout>
-      <h1>New Campaign!</h1>
+      <h3>Create A Campaign</h3>
+      <Form>
+        <Form.Field>
+          <label>Minimum Contribution</label>
+          <input type="text" />
+        </Form.Field>
+        <Button type="submit" primary>
+          Create
+        </Button>
+      </Form>
     </Layout>
   );
 };
