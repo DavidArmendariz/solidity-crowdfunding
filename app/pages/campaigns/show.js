79f5a4f4de8a180934e4fea0a9ee99f8af519522
David Armendariz <david.adrian.a.p@gmail.com>
2022-02-19 15:32:35 -0500

    Added layout to campaign show

---
+++ app/pages/campaigns/show.js
@@ -0,0 +1,12 @@
+import Layout from 'app-components/Layout';
+import React from 'react';
+
+const CampaignShow = () => {
+  return (
+    <Layout>
+      <h3>Campaign Details</h3>
+    </Layout>
+  );
+};
+
+export default CampaignShow;
