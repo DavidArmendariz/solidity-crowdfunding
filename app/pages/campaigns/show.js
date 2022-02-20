d94eccf8ea8b9a7a57ceb5445a07b2e3e7700ce4
David Armendariz <david.adrian.a.p@gmail.com>
2022-02-19 21:15:43 -0500

    Added campaign summary props to show component props via getInitialProps

--- app/pages/campaigns/show.js
+++ app/pages/campaigns/show.js
@@ -2,7 +2,13 @@ import React from 'react';
 import Layout from 'app-components/Layout';
 import getCampaign from 'app-utils/get-campaign';
 
-const CampaignShow = ({ campaign }) => {
+const CampaignShow = ({
+  minimumContribution,
+  balance,
+  numRequests,
+  contributorsCount,
+  manager,
+}) => {
   return (
     <Layout>
       <h3>Campaign Details</h3>
@@ -16,8 +22,13 @@ CampaignShow.getInitialProps = async (props) => {
   } = props;
   const campaign = getCampaign(address);
   const summary = await campaign.methods.getSummary().call();
-  console.log(summary);
-  return {};
+  return {
+    minimumContribution: summary[0],
+    balance: summary[1],
+    numRequests: summary[2],
+    contributorsCount: summary[3],
+    manager: summary[4],
+  };
 };
 
 export default CampaignShow;
