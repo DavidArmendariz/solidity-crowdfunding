5fc24cd1ea641ff208e963539522fa0f33d4611a
David Armendariz <david.adrian.a.p@gmail.com>
2022-02-21 20:38:50 -0500

    Added form for new request

--- app/pages/campaigns/requests/new.js
+++ app/pages/campaigns/requests/new.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form, Input, Message } from 'semantic-ui-react';
 import Layout from 'app-components/Layout';
+import { Link, Router } from 'app-routes';
+import web3 from 'app-web3';
+import Campaign from '../../../../ethereum/build/Campaign.json';
 
-const NewRequest = () => {
+const NewRequest = ({ address }) => {
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('0');
+  const [recipient, setRecipient] = useState('');
+
   return (
     <Layout>
       <h3>Create a Request</h3>
+      <Form>
+        <Form.Field>
+          <label>Description</label>
+          <Input
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Value (ether)</label>
+          <Input
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Recipient</label>
+          <Input
+            value={recipient}
+            onChange={(event) => setRecipient(event.target.value)}
+          />
+        </Form.Field>
+        <Button primary>Create</Button>
+      </Form>
     </Layout>
   );
+};
+
+NewRequest.getInitialProps = (props) => {
+  const { address } = props.query;
+  return {
+    address,
+  };
 };
 
 export default NewRequest;
